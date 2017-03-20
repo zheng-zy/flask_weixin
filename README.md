0145d251d61c882424e74ac028233b86cb693a05
zhengzy <zhezhiyong@163.com>
2017-03-21 00:04:38 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # flask_weixin
 个人微信订阅号
+
+demo：https://github.com/paicha/gxgk-wechat-server
+demo：https://github.com/cloverstd/flask-wechatpy/blob/master/demo.py
+sdk：https://github.com/jxtech/wechatpy/
+
+
+选型：
+flask+sdk+sqlite
+
+pip freeze > requirements.txt 
---
+++ app.py
@@ -0,0 +1,56 @@
+#!usr/bin/env python
+# coding=utf-8
+# Created by zhezhiyong@163.com on 2017/3/20.
+
+from flask import Flask, request, session
+from flask_wechatpy import Wechat, wechat_required, oauth
+from wechatpy.replies import TextReply
+from wechatpy.replies import create_reply
+
+app = Flask(__name__)
+app.config['WECHAT_APPID'] = 'wx3b602e650c2c8dda'
+app.config['WECHAT_SECRET'] = '12e75aabd90ab2e034941f61f0c8d0aa'
+app.config['WECHAT_TOKEN'] = 'token'
+app.config['DEBUG'] = True
+app.secret_key = '882f224578cf4043d6b440520fe97085'  # AppSecret
+# QCgz5h5kaixpc7Kb5gYyx7JXwYFRQlf439Bp9us4zZW EncodingAESKey
+
+wechat = Wechat(app)
+
+
+@app.route('/')
+@oauth(scope='snsapi_userinfo')
+def index():
+    return "hello"
+
+
+@app.route('/clear')
+def clear():
+    if 'wechat_user_id' in session:
+        session.pop('wechat_user_id')
+    return "ok"
+
+
+@app.route('/wechat', methods=['GET', 'POST'])
+@wechat_required
+def wechat_handler():
+    msg = request.wechat_msg
+    if msg.type == 'text':
+        reply = create_reply(msg.content, message=msg)
+    else:
+        reply = TextReply(content='hello', message=msg)
+
+    return reply
+
+
+@app.route('/access_token')
+def access_token():
+    return "access token: {}".format(wechat.access_token)
+
+
+# https://mp.weixin.qq.com/advanced/advanced?action=interface&t=advanced/interface&token=1269577494&lang=zh_CN
+# http://sumonian.ngrok.cc/access_token
+# token
+# QCgz5h5kaixpc7Kb5gYyx7JXwYFRQlf439Bp9us4zZW
+if __name__ == '__main__':
+    app.run('127.0.0.1', 5000)
