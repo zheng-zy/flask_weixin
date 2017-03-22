36ddd24527157284d842f1d2784b16a2a1f3b1fa
zhengzy <zhezhiyong@163.com>
2017-03-22 14:59:49 +0800

    token

--- app.py
+++ app.py
@@ -3,8 +3,10 @@
 # Created by zhezhiyong@163.com on 2017/3/20.
 import logging
 
-from flask import Flask, request
-from wechatpy.exceptions import InvalidSignatureException
+from flask import Flask, request, abort
+from wechatpy import parse_message, create_reply
+from wechatpy.crypto import WeChatCrypto
+from wechatpy.exceptions import InvalidSignatureException, InvalidAppIdException
 from wechatpy.utils import check_signature
 
 app = Flask(__name__)
@@ -15,28 +17,54 @@ app.config['DEBUG'] = True
 app.secret_key = '882f224578cf4043d6b440520fe97085'  # AppSecret
 # QCgz5h5kaixpc7Kb5gYyx7JXwYFRQlf439Bp9us4zZW EncodingAESKey
 
+APP_ID = app.config['WECHAT_APPID']
+TOKEN = app.config['WECHAT_TOKEN']
+ENCODING_AES_KEY = 'QCgz5h5kaixpc7Kb5gYyx7JXwYFRQlf439Bp9us4zZW'
+
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 logger = logging.getLogger()
+GET = 'GET'
+POST = 'POST'
+TEXT = 'text'
 
 
-@app.route('/')
-def index():
-    echostr = request.args.get('echostr', '')
+@app.route('/wechat', methods=[GET, POST])
+def wechat():
+    echo_str = request.args.get('echo_str', '')
     signature = request.args.get('signature', '')
     timestamp = request.args.get('timestamp', '')
     nonce = request.args.get('nonce', '')
-    logger.info('token: {%s}', app.config['WECHAT_TOKEN'])
+    encrypt_type = request.args.get('encrypt_type', '')
+    msg_signature = request.args.get('msg_signature', '')
+    logger.debug('echo_str: {%s}', echo_str)
+    logger.debug('signature: {%s}', signature)
+    logger.debug('timestamp: {%s}', timestamp)
+    logger.debug('nonce: {%s}', nonce)
+    logger.debug('encrypt_type: {%s}', encrypt_type)
+    logger.debug('msg_signature: {%s}', msg_signature)
     try:
-        check_signature(app.config['WECHAT_TOKEN'], signature, timestamp, nonce)
+        check_signature(TOKEN, signature, timestamp, nonce)
     except InvalidSignatureException:
         logger.error('check_signature error')
-        return
-    return echostr
+        abort(403)
+    if request.method == GET:
+        return echo_str
+    else:
+        logger.info('raw message: {%s}', request.data)
+        crypto = WeChatCrypto(TOKEN, ENCODING_AES_KEY, APP_ID)
+        msg = ''
+        try:
+            msg = crypto.decrypt_message(request.data, msg_signature, timestamp, nonce)
+            logger.debug('descypted message: {%s}', msg)
+        except (InvalidSignatureException, InvalidAppIdException):
+            abort(403)
+        msg = parse_message(msg)
+        if msg.type == TEXT:
+            reply = create_reply(msg.content, msg)
+        else:
+            reply = create_reply('sorry, can not handle this for now', msg)
+        return crypto.encrypt_message(reply.render(), nonce, timestamp)
 
 
-# https://mp.weixin.qq.com/advanced/advanced?action=interface&t=advanced/interface&token=1269577494&lang=zh_CN
-# http://sumonian.ngrok.cc/access_token
-# token
-# QCgz5h5kaixpc7Kb5gYyx7JXwYFRQlf439Bp9us4zZW
 if __name__ == '__main__':
     app.run('127.0.0.1', 5000)
