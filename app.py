e4773900418ac35a74e71c94ea438445416aa86c
zhengzy <zhezhiyong@163.com>
2017-03-22 14:35:00 +0800

    token

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 #!usr/bin/env python
 # coding=utf-8
 # Created by zhezhiyong@163.com on 2017/3/20.
+import logging
 
-from flask import Flask, request, session
-from flask_wechatpy import Wechat, wechat_required
+from flask import Flask, request
 from wechatpy.exceptions import InvalidSignatureException
-from wechatpy.replies import TextReply
-from wechatpy.replies import create_reply
 from wechatpy.utils import check_signature
 
 app = Flask(__name__)
@@ -17,50 +15,23 @@ app.config['DEBUG'] = True
 app.secret_key = '882f224578cf4043d6b440520fe97085'  # AppSecret
 # QCgz5h5kaixpc7Kb5gYyx7JXwYFRQlf439Bp9us4zZW EncodingAESKey
 
-wechat = Wechat(app)
-
-
-@app.route('/hello')
-def hello():
-    return "hello"
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+logger = logging.getLogger()
 
 
 @app.route('/')
-# @oauth(scope='snsapi_userinfo')
 def index():
     echostr = request.args.get('echostr', '')
     signature = request.args.get('signature', '')
     timestamp = request.args.get('timestamp', '')
     nonce = request.args.get('nonce', '')
+    logger.info('token: {}', app.config['WECHAT_TOKEN'])
     try:
         check_signature(app.config['WECHAT_TOKEN'], signature, timestamp, nonce)
     except InvalidSignatureException:
+        logger.error('check_signature error')
         return
     return echostr
-
-
-@app.route('/clear')
-def clear():
-    if 'wechat_user_id' in session:
-        session.pop('wechat_user_id')
-    return "ok"
-
-
-@app.route('/wechat', methods=['GET', 'POST'])
-@wechat_required
-def wechat_handler():
-    msg = request.wechat_msg
-    if msg.type == 'text':
-        reply = create_reply(msg.content, message=msg)
-    else:
-        reply = TextReply(content='hello', message=msg)
-
-    return reply
-
-
-@app.route('/access_token')
-def access_token():
-    return "access token: {}".format(wechat.access_token)
 
 
 # https://mp.weixin.qq.com/advanced/advanced?action=interface&t=advanced/interface&token=1269577494&lang=zh_CN
